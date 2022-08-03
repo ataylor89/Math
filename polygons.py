c0b3ae1e69eef214a020f33f693c466061023ae1
Andrew Taylor <rolyata47@gmail.com>
2022-08-03 13:53:48 -0500

    Updating comments and print messages

--- polygons.py
+++ polygons.py
@@ -3,30 +3,35 @@
 
 import math
 
+# Returns the area of an n-sided regular polygon inscribed in a unit circle as an approximation of pi
 def approximate_pi(n):
     A = n * math.sin(math.pi/n) * math.cos(math.pi/n)
     # The expression %-10d tells the print function to left-justify the integer by ten spaces
     # The expression %.20f tells the print function to display 20 decimal places of the float
     # This formatting syntax was adopted by Python from the C programming language
-    print("n: %-10d A: %.20f" %(n, A))
     return A
 
-def main():
-    print("========================================================================================")
-    print("A table of areas for n-sided regular polygons inscribed in a unit circle.")
-    print("The limit of the area of an n-sided regular polygon as n approaches infinity is pi.")
-    print("In limit notation, lim n->inf n*sin(pi/n)*cos(pi/n) = pi")
-    print("========================================================================================")
-    for n in range(3, 20, 1):
-        approximate_pi(n)
-    for n in range(20, 100, 10):
-        approximate_pi(n)
-    for n in range(1000, 10000, 1000):
-        approximate_pi(n)
-    for n in range(10000, 100000, 10000):
-        approximate_pi(n)
-    for n in range(100000, 1000000, 100000):
-        approximate_pi(n)
+# Prints a table of areas for n-sided regular polygons inscribed in a unit circle.
+# The limit of the area of an n-sided regular polygon as n approaches infinity is pi.
+# In limit notation, lim n->inf n*sin(pi/n)*cos(pi/n) = pi 
+def table(begin, end, step):
+    for n in range(begin, end, step):
+        print("%-10d %.20f" %(n, approximate_pi(n)))
 
+# Prints a table of areas for n-sided regular polygons inscribed in a unit circle
+def main():
+    print("Area of a regular n-sided polygon inscribed in a unit circle (approximation of pi)")
+    print("==================================================================================")
+    print("%-10s %s" %("n", "A"))
+    print("==================================================================================")
+    table(3, 20, 1)
+    table(20, 100, 10)
+    table(100, 1000, 100)
+    table(1000, 10000, 1000)
+    table(10000, 100000, 10000)
+
+# This code will only run if it's the top-level environment (the entry point to the application)
+# https://docs.python.org/3/library/__main__.html
+# In other words, if the file is imported, this code will not be run
 if __name__ == "__main__":
     main()
